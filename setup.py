aaa828fe10a1114b08d43f49a36944bd399d0e01
Umar Javed <javedumar507@gmail.com>
2024-10-12 20:47:44 +0500

    updated documentations

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name="FastGenAPI",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "aiocache==0.12.3",
